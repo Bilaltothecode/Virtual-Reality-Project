a82ce58a86bedee3f0fa0e6634fb676abf792163
Anonymous <anonymous@example.com>
2022-05-03 16:41:29 -0600

    Scene 2 (SteamVR)

---
+++ Assets/MVP/Scripts/Scene2WeightChecker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene2WeightChecker : WeightChecker
+{
+    [SerializeField]
+    Animator fadeAnimator;
+
+    protected override void HitTarget()
+    {
+        StartCoroutine(SwitchScene());
+    }
+
+    IEnumerator SwitchScene()
+    {
+        yield return new WaitForSeconds(2);
+        fadeAnimator.SetBool("Faded", true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("3");
+    }
+}
